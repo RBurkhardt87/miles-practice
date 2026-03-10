4bdde141327433595f56bb4ac8ad49ce84843d64
Your Name <you@example.com>
2026-03-09 20:40:00 -0500

    wrote Miles simple instructions for Hello World project

--- helloWorld.js
+++ helloWorld.js
@@ -1,1 +1,15 @@
-console.log("hello world");
+/* hello world is usually everyone's first project. It shows us how to use the console.log() function to make text appear in the terminal
+when running the file. We will open a git bash terminal and make sure we are in the correct directory. After we are, we will use node.js 
+commands to execute the program file. And you will get to see what appears in the terminal. We can then practice with a few more things.
+
+NOTE: I have already downloaded node.js and git bash to make it easier, but if we end up downloading Visual Studio Code (VSC) on your 
+computer we can work through it all together. In the meantime, you can use my laptop to work through the first JavaScript (JS) lessons.
+
+
+Now, let's begin. First, I would like you to write out the console.log() function. Anything you put in the parenthesis will print to the 
+terminal console, but everything must be in quotes to become a printable string. 
+
+
+Try printing Hello, World!
+*/
+
